3a7e326777aaf80dc0daa9ca9ffe6526cd4de392
joha4022 <joha4022@gmail.com>
2023-05-15 17:39:28 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const searchBox = document.querySelector(['data-search']);
+
+searchBox.addEventListener('input', e => {
+    const value = e.target.value
+    console.log(value);
+})
+
+function search() {
+    
+}
