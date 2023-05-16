4eafd846a2d679f12ddf60bccc1a80f258eac982
juju <joha4022@gmail.com>
2023-05-15 17:59:06 -0700

    made comments

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const searchBox = document.querySelector(['data-search']);
 
+// add event listener to collect the input
 searchBox.addEventListener('input', e => {
     const value = e.target.value
     console.log(value);
 })
 
+// create a search function and search the document
 function search() {
     
 }
